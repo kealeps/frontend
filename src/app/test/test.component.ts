c0f91a75c514b8caa1d38f1660a1a3a65ccb5ab0
Kevin Perez <mr.kv17.kp@gmail.com>
2021-10-10 20:28:26 -0400

    first commit

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { TestService } from './test.service';
+
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.scss']
+})
+export class TestComponent implements OnInit {
+
+  constructor(private test: TestService) { }
+
+  ngOnInit(): void {
+    this.test.test().subscribe( response => {
+      console.log(response);
+      
+    })
+  }
+
+}
---
+++ src/app/test/test.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TestService {
+
+  constructor(private http: HttpClient) { }
+
+  test(): Observable<any>{
+    return this.http.get("https://backend-test-heroku.herokuapp.com/hello")
+  }
+}
